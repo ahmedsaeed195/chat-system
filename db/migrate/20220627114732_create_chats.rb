a10f8fa3222abc20e54cb74df095db932746fd5e
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-06-28 00:58:13 +0200

    added reject null to some columns

--- db/migrate/20220627114732_create_chats.rb
+++ db/migrate/20220627114732_create_chats.rb
@@ -1,8 +1,8 @@
 class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
-      t.integer :chat_no
-      t.references :application, foreign_key: true, dependent: :destroy
+      t.integer :chat_no, null: false
+      t.references :application, foreign_key: true, null: false, dependent: :destroy
       t.integer :messages_count, default: 0
 
       t.timestamps
