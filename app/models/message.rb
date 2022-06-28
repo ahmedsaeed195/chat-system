b091febedf10444b4c8f9e9d32c63b084f87a62a
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-06-28 18:30:54 +0200

    added messages controller and elasticsearch

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,42 @@
 class Message < ApplicationRecord
   belongs_to :chat, counter_cache: true
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mapping dynamic: 'false' do
+      indexes :id, type: :integer
+      indexes :message_no, type: :integer
+      indexes :sender
+      indexes :content
+      indexes :chat_id, type: :integer
+      indexes :created_at, type: :date, format: :date_optional_time
+      indexes :updated_at, type: :date, format: :date_optional_time
+    end
+  end
+
+  # def self.search(query)
+  #   __elasticsearch__.search(
+  #     {
+  #       query: {
+  #         match: {
+  #           query: { match: { content: { query: query, operator: 'and' } } },
+  #           fields: ['content']
+  #         }
+  #       }
+  #     }
+  #   )
+  # end
+
+  # Delete the previous articles index in Elasticsearch
+  begin
+    Message.__elasticsearch__.delete_index!
+  rescue StandardError
+    nil
+  end
 end
+# Create the new index with the new mapping
+Message.__elasticsearch__.create_index!
+# Index all article records from the DB to Elasticsearch
+Message.import
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :application, controller: :applications, param: :token do
-    resources :chat, controller: :chats, param: :chat_no
+    resources :chat, controller: :chats, param: :no do
+      resources :message, controller: :messages, param: :message_no
+      get 'messages/search' => 'messages#search'
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
